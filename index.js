06cc4734a91d3d272b8ed098fda394f51c90f0d6
Bhargav Vanra <88825575+Bhargav007SGL@users.noreply.github.com>
2023-02-21 08:51:58 +0000

    did ome changes

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 let fileSystem = require("fs");
 let ndjson = require("ndjson");
-let finalOutput = [];
 let records = [
   { id: 1, name: "O Brother, Where Art Thou?" },
   { id: 2, name: "Home for the Holidays" },
   { id: 3, name: "The Firm" },
   { id: 4, name: "Broadcast News" },
   { id: 5, name: "Raising Arizona" },
-  // .... hundreds of thousands of records ....
 ];
 let transformStream = ndjson.stringify();
 let outputStream = transformStream.pipe(
@@ -22,17 +20,18 @@ outputStream.on("finish", function handleFinish() {
   console.log("- - - - - - - - - - - - - - - - - - - - - - -");
 });
 outputStream.on("finish", function handleFinish() {
+  let finalOutput = `[`;
   let inputStream = fileSystem.createReadStream(__dirname + "/data.ndjson");
-  let transformStream = inputStream.pipe(ndjson.parse());
-
-  transformStream
+  // let transformStream = inputStream.pipe(ndjson.parse());
+  inputStream
     .on("data", function handleRecord(data) {
       console.log("Record (event):", data);
-      finalOutput.push(data);
+      finalOutput += data;
     })
     .on("end", function handleEnd() {
       console.log("- - - - - - - - - - - - - - - - - - - - - - -");
       console.log("ndjson parsing complete!");
+      finalOutput += `]`;
       console.log(finalOutput);
     });
 });
